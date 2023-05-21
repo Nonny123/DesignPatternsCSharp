df1c77f84f840d1a96503bef4f02504c6398824e
Nonso <justcoolk@yahoo.com>
2023-05-21 10:51:37 +0100

    object oriented concepts

---
+++ designpatterns/designpatterns/DesignPrinciples/3. LiskovSubstitution/EmployeeManagement/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace designpatterns.DesignPrinciples._3._LiskovSubstitution.EmployeeManagement
+{
+    class Employee
+    {
+    }
+}
